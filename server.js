971c1c540a46646c70809cb860f132fbe63ff87d
jaygroover1 <jaygroover@gmail.com>
2019-05-20 00:03:32 -0400

    4th update

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.use(bodyParser.raw({ type: 'application/vnd.custom-type'}))
 
 app.use(bodyParser.text({ type: 'text/html'}))
 
+require("./app/routing/html-routes.js")(app);
+
 app.listen(PORT, function(){
     console.log("App listening on PORT: " + PORT);
 });
