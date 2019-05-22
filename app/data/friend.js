f3f39f50139afe465fffd93424e9900518be1e77
jaygroover1 <jaygroover@gmail.com>
2019-05-22 17:09:06 -0400

    5th update

---
+++ app/data/friend.js
@@ -0,0 +1,56 @@
+
+var friends = [
+    {
+        name: "Tony Stark",
+        Photo:"https://www.thewealthrecord.com/wp-content/uploads/2018/09/Tony-Stark-Net-Worth-2019.jpeg",
+        scores:[
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+
+    {
+       name: "Hulk",
+       photo:"https://www.gannett-cdn.com/-mm-/a41905a88e2dacc6746b754b4f6af4cef04b6895/c=421-0-2092-1257/local/-/media/2017/10/30/USATODAY/USATODAY/636449735557748089-ThorRagnarok5974d7a2aceff.jpg?width=540&height=405&fit=crop",
+       scores:[
+           "4",
+           "3",
+           "4",
+           "1",
+           "5",
+           "2",
+           "5",
+           "3",
+           "1",
+           "4"
+       ] 
+    },
+
+    {
+        name: "Black Widow",
+        photo:"https://cdn.vox-cdn.com/thumbor/o0YrLQ-5RUrhfGGnWdJ0e6L13f4=/0x0:1280x640/2120x1413/filters:focal(580x195:784x399):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/60256343/black-widow.0.jpg",
+        scores:[
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    }
+]
+
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -6,9 +6,7 @@ var bodyParser = require('body-parser');
 
 var PORT = process.env.PORT || 8080;
 
-var jsonParser = bodyParser.json();
-
-var urlencodedParser = bodyParser.urlencoded({extended:false});
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(bodyParser.json({type: 'application/*+json'}))
 
@@ -16,6 +14,7 @@ app.use(bodyParser.raw({ type: 'application/vnd.custom-type'}))
 
 app.use(bodyParser.text({ type: 'text/html'}))
 
+require("./app/routing/api-routes.js")(app);
 require("./app/routing/html-routes.js")(app);
 
 app.listen(PORT, function(){
