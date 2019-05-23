a90b57605e5e1afe27835640bec129371a8b25cc
jaygroover1 <jaygroover@gmail.com>
2019-05-22 22:03:52 -0400

    new questions

--- app/data/friend.js
+++ app/data/friend.js
@@ -5,15 +5,15 @@ var friends = [
         Photo:"https://www.thewealthrecord.com/wp-content/uploads/2018/09/Tony-Stark-Net-Worth-2019.jpeg",
         scores:[
             "5",
-            "1",
+            "5",
             "4",
             "4",
             "5",
-            "1",
-            "2",
+            "5",
+            "3",
             "5",
             "4",
-            "1"
+            "4"
         ]
     },
 
@@ -21,16 +21,16 @@ var friends = [
        name: "Hulk",
        photo:"https://www.gannett-cdn.com/-mm-/a41905a88e2dacc6746b754b4f6af4cef04b6895/c=421-0-2092-1257/local/-/media/2017/10/30/USATODAY/USATODAY/636449735557748089-ThorRagnarok5974d7a2aceff.jpg?width=540&height=405&fit=crop",
        scores:[
-           "4",
-           "3",
-           "4",
            "1",
-           "5",
            "2",
-           "5",
            "3",
            "1",
-           "4"
+           "1",
+           "2",
+           "3",
+           "1",
+           "1",
+           "3"
        ] 
     },
 
@@ -49,7 +49,26 @@ var friends = [
             "4",
             "5"
         ]
+    },
+
+    {
+    name: "Thanos",
+    photo: "https://cdn.vox-cdn.com/thumbor/HcrcKEuM1VNk8bvIqfd6o2ZBd0M=/249x0:2371x1048/2120x1413/filters:focal(1073x320:1481x728):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/60323187/Screen_Shot_2018_07_10_at_1.52.58_PM.0.png",
+    scores:[
+        "1",
+        "5",
+        "3",
+        "5",
+        "2",
+        "1",
+        "3",
+        "1",
+        "2",
+        "5"
+    ]
+
     }
+
 ]
 
 
